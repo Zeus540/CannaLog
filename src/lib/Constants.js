7bc625c56402f98e76882a9a892af403f25c86d8
Zeus540 <zaheerroberts4@gmail.com>
2023-07-24 00:04:56 +0200

    updated

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -2,4 +2,4 @@
 //export const BASE_URL_PROD_SOCKET= "http://localhost:10002"
 
 export const BASE_URL_PROD = "https://api.cannalog.co.za"
-export const BASE_URL_PROD_SOCKET= "https://api.cannalog.co.za"
+export const BASE_URL_PROD_SOCKET= "https://api.cannalog.co.za" 
