775673c012fb0e3289589a9fb4c1f736ad5e833c
Zeus540 <zaheerroberts4@gmail.com>
2023-07-23 20:08:27 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), reactRefresh()],
-  build: {
-    rollupOptions: {
-      external: ['@emotion/react'],
-    },
-  },
 })
