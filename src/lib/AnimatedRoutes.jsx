420fd1bd7fe2768e7f72e560253c323eec8cfea8
Zeus540 <zaheerroberts4@gmail.com>
2023-10-23 12:53:51 +0200

    added lazy routes

--- src/lib/AnimatedRoutes.jsx
+++ src/lib/AnimatedRoutes.jsx
@@ -1,47 +1,76 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,lazy } from 'react'
 import styled from 'styled-components';
-import PreLoader from '../components/preLoader/PreLoader'
-import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
 import { Routes, Route } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
-import HomePage from '../pages/HomePage/HomePage'
 import { SnackbarProvider } from 'notistack'
-import Terms from '../pages/Terms/Terms';
-import CookiePolicy from '../pages/CookiePolicy/CookiePolicy';
-import PrivacyPolicy from '../pages/PrivacyPolicy/PrivacyPolicy';
-import Growers from '../pages/Growers/Growers';
-import Notifications from '../pages/Notifications/Notifications';
-import PublicPlants from '../pages/PublicPlants/PublicPlants';
+import { useSelector } from 'react-redux';
+
+//Pages
+// import HomePage from '../pages/HomePage/HomePage'
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
+// import RegistrationComplete from '../pages/Register/RegistrationComplete'
+// import Verify from '../pages/Register/Verify'
+// import Terms from '../pages/Terms/Terms';
+// import CookiePolicy from '../pages/CookiePolicy/CookiePolicy';
+// import PrivacyPolicy from '../pages/PrivacyPolicy/PrivacyPolicy';
+// import Growers from '../pages/Growers/Growers';
+// import Notifications from '../pages/Notifications/Notifications';
+// import PublicPlants from '../pages/PublicPlants/PublicPlants';
+// import Login from '../pages/Login/Login'
+// import Register from '../pages/Register/Register'
+// import Environments from '../pages/Environments/Environments'
+// import MyPlants from '../pages/MyPlants/MyPlants'
+// import MyPlantsDetailed from '../pages/MyPlants/MyPlantsDetailed'
+// import PublicPlantDetailed from '../pages/PublicPlants/PublicPlantDetailed';
+
+//Context
 import { SocketProvider } from '../context/SocketContext';
 import { NotificationProvider } from '../context/NotificationContext';
+import { LogoutProvider } from '../context/LogoutContext';
+
+//Features
 import {
     selectIsLoggedIn,
   } from '../features'
-import Login from '../pages/Login/Login'
-import Register from '../pages/Register/Register'
-import RegistrationComplete from '../pages/Register/RegistrationComplete'
-import Verify from '../pages/Register/Verify'
+
+//Components
 import NavBar from '../components/navbar/NavBar'
-import Environments from '../pages/Environments/Environments'
-import MyPlants from '../pages/MyPlants/MyPlants'
-import MyPlantsDetailed from '../pages/MyPlants/MyPlantsDetailed'
 import Footer from '../components/footer/Footer'
 import WebSocketListener from '../components/WebSocketListener';
-import PublicPlantDetailed from '../pages/PublicPlants/PublicPlantDetailed';
+
+//Utils
 import ProtectedRoutes from '../utils/ProtectedRoutes';
-import { LogoutProvider } from '../context/LogoutContext';
-import { useSelector } from 'react-redux';
+import LazyRoutes from '../utils/LazyRoutes';
+
 
 const Root = styled.div`
-
 background:  ${props => props.theme.secondary};
 `;
+
 
 function AnimatedRoutes({themeType,toggleTheme}) {
     const isLoggedIn = useSelector(selectIsLoggedIn)
     const location = useLocation()
     const [mobileMenu, setMobileMenu] = useState(false);
+
+
+    
+    const HomePage = lazy(()=>import('../pages/HomePage/HomePage'))
+    const Notifications = lazy(()=>import('../pages/Notifications/Notifications'))
+    const Growers = lazy(()=>import('../pages/Growers/Growers'))
+    const MyPlants = lazy(()=>import('../pages/MyPlants/MyPlants'))
+    const MyPlantsDetailed = lazy(()=>import('../pages/MyPlants/MyPlantsDetailed'))
+    const PublicPlants = lazy(()=>import('../pages/PublicPlants/PublicPlants'))
+    const PublicPlantDetailed = lazy(()=>import('../pages/PublicPlants/PublicPlantDetailed'))
+    const Environments = lazy(()=>import('../pages/Environments/Environments'))
+    const Terms = lazy(()=>import('../pages/Terms/Terms'))
+    const CookiePolicy = lazy(()=>import('../pages/CookiePolicy/CookiePolicy'))
+    const PrivacyPolicy = lazy(()=>import('../pages/PrivacyPolicy/PrivacyPolicy'))
+    const Login = lazy(()=>import('../pages/Login/Login'))
+    const Register = lazy(()=>import('../pages/Register/Register'))
+    const RegistrationComplete = lazy(()=>import('../pages/Register/RegistrationComplete'))
+    const Verify = lazy(()=>import('../pages/Register/Verify'))
 
     const OffClick = () => {
         if (mobileMenu == true) {
@@ -49,14 +78,14 @@ function AnimatedRoutes({themeType,toggleTheme}) {
         }
     }
 
-
     useEffect(() => {
 
         window.scrollTo(0, 0);
         
         window.onunload = function () {
             window.scrollTo(0, 0);
-          }
+        }
+
       }, [location]);
 
     
@@ -80,28 +109,28 @@ function AnimatedRoutes({themeType,toggleTheme}) {
                             <Route path="/growers" element={<Growers />} />
                         </Route>
                      
-           
-                        <Route path="/public-plant/:plant_name/:environment_id/:plant_id" element={<PublicPlantDetailed />} />
-                        <Route path="/terms" element={<Terms />} />
-                        <Route path="/cookie-policy" element={<CookiePolicy />} />
-                        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-
-
-                        {!isLoggedIn ?
-                        <>
-                           <Route path="/" element={<HomePage />} />
-                           <Route path="/public-plants" element={<PublicPlants />} />
-                            <Route path="/sign-in" element={<Login />} />
-                            <Route path="/sign-up" element={<Register />} />
-                            <Route path="/sign-up/:name/:email" element={<RegistrationComplete />} />
-                            <Route path="/verify/:token" element={<Verify />}/>
-                        </>
-                        :
-                        <>
-                            <Route path="/" element={<PublicPlants />} />
-                        </>
-                        }
+                        <Route element={<LazyRoutes/>}>    
+                            <Route path="/public-plant/:plant_name/:environment_id/:plant_id" element={<PublicPlantDetailed />} />
+                            <Route path="/terms" exact element={<Terms />} />
+                            <Route path="/cookie-policy" element={<CookiePolicy />} />
+                            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                            {!isLoggedIn ?
+                            <>
                         
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/public-plants" element={<PublicPlants />} />
+                                <Route path="/sign-in" element={<Login />} />
+                                <Route path="/sign-up" element={<Register />} />
+                                <Route path="/sign-up/:name/:email" element={<RegistrationComplete />} />
+                                <Route path="/verify/:token" element={<Verify />}/>
+                        
+                            </>
+                            :
+                            <>
+                                <Route path="/" element={<PublicPlants />} />
+                            </>
+                            }
+                         </Route>
                         <Route path="*" element={<NotFoundPage />}/>
                     </Routes>
 
--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -1,5 +1,12 @@
 import { Outlet, Navigate,useLocation } from "react-router"
 import { useCookies } from "react-cookie"
+import { Suspense } from "react"
+import styled from 'styled-components';
+
+const Root = styled.div`
+background:  ${props => props.theme.secondary};
+min-height:calc(100vh - 180px)
+`;
 
 const ProtectedRoutes = () => {
     const cookies = useCookies()
@@ -8,7 +15,7 @@ const ProtectedRoutes = () => {
     let auth = cookies[0].user == undefined ? false : true
     
     return (
-        auth ? <Outlet></Outlet> : <Navigate to="/sign-in" state={location.pathname}/>
+        auth ?  <Root><Suspense fallback="...loading"><Outlet></Outlet></Suspense></Root> : <Navigate to="/sign-in" state={location.pathname}/>
     )
 }
 
